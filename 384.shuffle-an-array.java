fc5444dfcec5c6ff19ab23f19efa8c65dc48baf1
472120366 <472120366@qq.com>
2018-12-21 15:48:34 -0800

    1221

---
+++ 384.shuffle-an-array.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+
+/*
+ * @lc app=leetcode id=384 lang=java
+ *
+ * [384] Shuffle an Array
+ *
+ * https://leetcode.com/problems/shuffle-an-array/description/
+ *
+ * algorithms
+ * Medium (48.81%)
+ * Total Accepted:    61.8K
+ * Total Submissions: 126.5K
+ * Testcase Example:  '["Solution","shuffle","reset","shuffle"]\n[[[1,2,3]],[],[],[]]'
+ *
+ * Shuffle a set of numbers without duplicates.
+ * 
+ * 
+ * Example:
+ * 
+ * // Init an array with set 1, 2, and 3.
+ * int[] nums = {1,2,3};
+ * Solution solution = new Solution(nums);
+ * 
+ * // Shuffle the array [1,2,3] and return its result. Any permutation of
+ * [1,2,3] must equally likely to be returned.
+ * solution.shuffle();
+ * 
+ * // Resets the array back to its original configuration [1,2,3].
+ * solution.reset();
+ * 
+ * // Returns the random shuffling of array [1,2,3].
+ * solution.shuffle();
+ * 
+ * 
+ */
+class Solution {
+    Random random;
+    int[] nums;
+    public Solution(int[] nums) {
+        random = new Random();
+        this.nums = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        if(nums == null) return null;
+        int[] copy = nums.clone();
+        for (int i = 1; i < copy.length; i++) {
+            
+            int ran = random.nextInt(i + 1);
+            int tmp = copy[ran];
+            copy[ran] = copy[i];
+            copy[i] = tmp;
+        }
+        return copy;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
