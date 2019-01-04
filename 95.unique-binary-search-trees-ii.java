9a59fa68c7de2031bc1f5d3e0481c391dad995a2
472120366 <472120366@qq.com>
2019-01-03 17:46:38 -0800

    123

---
+++ 95.unique-binary-search-trees-ii.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=95 lang=java
+ *
+ * [95] Unique Binary Search Trees II
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (34.20%)
+ * Total Accepted:    122.6K
+ * Total Submissions: 358.5K
+ * Testcase Example:  '3'
+ *
+ * Given an integer n, generate all structurally unique BST's (binary search
+ * trees) that store values 1 ... n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output:
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return get(1, n);
+    }
+    private List<TreeNode> get(int s, int e) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+
+        if (s > e) {
+            list.add(null);
+            return list;
+        }
+        for (int i = s; i <= e; i++) {
+            List<TreeNode> left = get(s, i - 1);
+            List<TreeNode> right = get(i + 1, e);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ 96.unique-binary-search-trees.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=96 lang=java
+ *
+ * [96] Unique Binary Search Trees
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees/description/
+ *
+ * algorithms
+ * Medium (44.27%)
+ * Total Accepted:    176.5K
+ * Total Submissions: 398.8K
+ * Testcase Example:  '3'
+ *
+ * Given n, how many structurally unique BST's (binary search trees) that store
+ * values 1 ... n?
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output: 5
+ * Explanation:
+ * Given n = 3, there are a total of 5 unique BST's:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ */
+class Solution {
+    public int numTrees(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[i - j - 1] * dp[j];
+            }
+        }
+        return dp[n];
+    }
+}
